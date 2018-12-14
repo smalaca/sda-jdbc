a3191afb8ee85226a0676953120e99fc115fcb9e
smalaca <ab13.krakow@gmail.com>
2018-12-14 18:27:02 +0100

    initial commit with jdbc connectivity

---
+++ src/main/java/com/smalaca/sda/ShopApp.java
@@ -0,0 +1,151 @@
+package com.smalaca.sda;
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ShopApp {
+    static final String DB_URL =
+            "jdbc:mysql://localhost/java_krk_13";
+    static final String USER = "jkrk13";
+    static final String PASS = "jkrk13";
+
+    public static void main( String[] args ) {
+
+        //register mysql driver
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // we should not continue
+        }
+
+        //establish connection
+        Connection connection = null;
+
+        try {
+            connection =
+                    DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //query execution
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE PRODUCTS " +
+                    "SET DESCRIPTION = ? WHERE PRODUCT_ID = ?"
+            );
+            updateProducts(preparedStatement);
+
+            statement = connection.createStatement();
+            insertProduct(statement);
+            findAllProducts(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                if (statement != null)
+                    statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //close connection
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateProducts(PreparedStatement preparedStatement) {
+        try {
+            preparedStatement.setString(1, "bla bla bla");
+            preparedStatement.setInt(2, 1);
+
+            int updated = preparedStatement.executeUpdate();
+
+            System.out.println(updated + " updated products.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertProduct(Statement statement) {
+        try {
+            int inserted = statement.executeUpdate(
+                    "INSERT INTO PRODUCTS VALUES " +
+                    "(1, '123456', 'first product', 'some description')"
+            );
+
+            System.out.println(inserted + " new products added.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void findAllProducts(Statement statement) {
+        ResultSet resultSet = null;
+
+        try {
+            resultSet = statement.
+                    executeQuery("SELECT product_id, name, description FROM PRODUCTS");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("product_id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+
+                System.out.println(
+                        "Product with id: " + id +
+                        " and name: " + name +
+                        " and description: " + description + "."
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/com/smalaca/sda/ShopAppTest.java
@@ -0,0 +1,12 @@
+package com.smalaca.sda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ShopAppTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
