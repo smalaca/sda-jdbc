a8420547adbd85c8e6984a0c611d353666f25d7e
smalaca <ab13.krakow@gmail.com>
2019-01-12 13:00:39 +0100

    MongoDb - Update operation

--- src/main/java/com/smalaca/sda/ShopApp.java
+++ src/main/java/com/smalaca/sda/ShopApp.java
@@ -1,16 +1,11 @@
 package com.smalaca.sda;
 
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
 import com.smalaca.sda.mongodb.MongoClientConnectivity;
 import com.smalaca.sda.mongodb.MongoCollections;
 import org.bson.Document;
-import org.bson.conversions.Bson;
-
-import java.util.Arrays;
 
 public class ShopApp {
 
@@ -20,21 +15,13 @@ public class ShopApp {
 
         MongoCollection<Document> books = new MongoCollections(mongoClientConnectivity).getCollection("books");
 
-        Bson criteria = Filters.in("categories", Arrays.asList("software development"));
-
-        FindIterable<Document> documents = books
-                .find(criteria)
-                .sort(Sorts.orderBy(
-                        Sorts.ascending("author"),
-                        Sorts.ascending("title"))
+        books.updateMany(
+                Filters.eq("description", "was great"),
+                Updates.combine(
+                        Updates.set("publish year", 2000),
+                        Updates.addToSet("categories", "anthem")
                 )
-                .projection(
-                        Projections.fields(
-                                Projections.exclude("author", "title")));
-
-        for (Document document : documents) {
-            System.out.println(document);
-        }
+        );
 
         System.out.println("----------------------------------");
         System.out.println("----------------------------------");
